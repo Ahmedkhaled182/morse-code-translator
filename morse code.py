3a054961194bb6411a27b180a80a3c4d012fd3ca
Ahmedkhaled182 <145559839+Ahmedkhaled182@users.noreply.github.com>
2023-11-28 23:04:43 +0200

    Update morse code.py

--- morse code.py
+++ morse code.py
@@ -27,3 +27,22 @@ morse_code_dict = {
     'z': '--..',
     ' ': ' '
 }
+
+def encrypt_morse_code(text):
+    encrypted_text = ''
+    for char in text:
+        if char in morse_code_dict:
+            encrypted_text += morse_code_dict[char] + ' '
+        else:
+            print(f"Unsupported character: {char}")
+    return encrypted_text
+
+def decode_morse_code(morse_code):
+    decoded_text = ''
+    morse_code_words = morse_code.split(' ')
+    for morse_code_word in morse_code_words:
+        for key, value in morse_code_dict.items():
+            if morse_code_word == value:
+                decoded_text += key
+                break
+    return decoded_text
