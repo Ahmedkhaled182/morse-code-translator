7beb98ea93a40c5ee5cf436e0f3d856a5700c2b7
Ahmedkhaled182 <145559839+Ahmedkhaled182@users.noreply.github.com>
2023-11-29 19:53:43 +0200

    Update morse code.py

--- morse code.py
+++ morse code.py
@@ -25,7 +25,7 @@ morse_code_dict = {
     'x': '-..-',
     'y': '-.--',
     'z': '--..',
-    ' ': ' '
+    ' ': '/'
 }
 
 def encrypt_morse_code(text):
@@ -37,6 +37,28 @@ def encrypt_morse_code(text):
             print(f"Unsupported character: {char}")
     return encrypted_text
 
+def decode_morse_code(morse_code):
+    decoded_text = ''
+    morse_code_words = morse_code.split(' ')
+    for morse_code_word in morse_code_words:
+        for key, value in morse_code_dict.items():
+            if morse_code_word == value:
+                decoded_text += key
+                break
+    return decoded_text
+
+user_input = input("Please enter text to encrypt or Morse code to decode: ")
+mode = input("Enter 'encrypt' to encrypt text or 'decode' to decode Morse code: ")
+
+if mode.lower() == 'encrypt':
+    encrypted_text = encrypt_morse_code(user_input.lower())
+    print("Encrypted text:", encrypted_text)
+elif mode.lower() == 'decode':
+    decoded_text = decode_morse_code(user_input)
+    print("Decoded text:", decoded_text)
+else:
+    print("Invalid mode. Please enter 'encrypt' or 'decode'.")
+    
 def decode_morse_code(morse_code):
     decoded_text = ''
     morse_code_words = morse_code.split(' ')
